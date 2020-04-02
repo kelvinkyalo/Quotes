8aaa5e58da3dbf81476ec356c42dfa6bc28c554b
kelvinkyalo <kelvinchax@gmail.com>
2020-04-02 16:10:07 +0300

    add form component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,13 @@
-export class Quote {
-    public showAuthor: boolean;
-    constructor(public id: number,public name: string,public quoteby: string,public uploadby:string,public completeDate: Date) {
-      this.showAuthor=false;
-    }
-  }
- 
+// export class Quote {
+//     public showAuthor: boolean;
+//     constructor(public id: number,public name: string,public quoteby: string,public uploadby:string,public completeDate: Date) {
+//       this.showAuthor=false;
+//     }
+//   }
+ export class Quote{
+   id:number;
+   name:string;
+   quoteby:string;
+   uploadby:string;
+   completeDate:Date;
+ }
