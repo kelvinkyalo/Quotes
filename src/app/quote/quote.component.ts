0b24002363db48f5f82211b6878eee9dba55da72
kelvinkyalo <kelvinchax@gmail.com>
2020-04-02 23:09:25 +0300

    add quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
   {id:1,name:"The greatest glory in living lies not in never falling, but in rising every time we fall.",quoteby: "Author-Nelson Mandela",uploadby: "Nelson",completeDate: new Date(2020,3,14)},
   {id:2,name:"The way to get started is to quit talking and begin doing.",quoteby: "Author -Walt Disney",uploadby: "Walt",completeDate:new Date(2020,3,14)},
-  {id:3,name:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",quoteby: "Author-Steve Jobs", uploadby:"Jobs",completeDate:new Date(2020,3,14)},
+  {id:3,name:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma –which is living with the results of other people's thinking.",quoteby: "Author-Steve Jobs", uploadby:"Jobs",completeDate:new Date(2020,3,14)},
   {id:4,name:"If life were predictable it would cease to be life, and be without flavor.",quoteby: "Author-Elenor Roosevelt",uploadby: "Roosevelt",completeDate:new Date(2020,3,14)},
   {id:5,name:"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",quoteby: "Author -Ophrah Winfrey",uploadby: "Winfrey",completeDate:new Date(2020,3,14)},
   {id:6,name:"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",quoteby: "Author by-James cameron",uploadby: "Cameron",completeDate:new Date(2020,3,14)},
